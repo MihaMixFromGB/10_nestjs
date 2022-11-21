86a4295376e31f4b24057fd4a0b23fc4f59e70b5
MihaMixFromGB <mihamix@yandex.ru>
2022-11-21 22:48:20 +0700

    Lesson 2 - Undo changes for root folders

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 import { NestExpressApplication } from '@nestjs/platform-express';
-// import { join } from 'path';
+import { join } from 'path';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // app.useStaticAssets(join(__dirname, '..', 'public'));
-  // app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.useStaticAssets('/public');
-  app.useStaticAssets('/views');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
   app.useGlobalPipes(
