eeda1e05b866957910e2d741c990af182c554587
MihaMixFromGB <mihamix@yandex.ru>
2022-11-26 20:23:26 +0700

    Lesson 3 - Changed path for static assets

--- api/vercel.ts
+++ api/vercel.ts
@@ -12,9 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.useStaticAssets(join(__dirname, '..', 'assets/swagger-ui-dist/'), {
-    prefix: '/api',
-  });
+  app.useStaticAssets(join(__dirname, '..', 'assets/swagger-ui-dist/'));
   app.engine('pug', pug.__express);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
